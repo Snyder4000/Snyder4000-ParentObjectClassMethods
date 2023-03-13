9b1baa6758c87bd04357ffab93553a8b81246597
Mark Snyder <Snyder4000@yahoo.com>
2023-03-13 12:47:47 +0000

    Completed Lab ParentObjectClassMethods

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -56,9 +56,18 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
-
+    public boolean equals(SampleClass other){
+        if(this.a == other.a && this.b == other.b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     //    implement a custom .toString(){} method here.
-
+    public String toString(){
+        return this.a + ", " + this.b;
+    }
     
 }
